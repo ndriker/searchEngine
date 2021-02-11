662bb140324a489632d7b7110f4e6a01be5c2f5c
michaellofton <28902859+michaellofton@users.noreply.github.com>
2021-02-11 14:46:27 -0800

    adding new file to start off of

---
+++ source/main.py
@@ -0,0 +1,12 @@
+def start():
+	pass
+
+# Tokens: all alphanumeric sequences in the dataset.
+
+# Stop words: do not use stopping while indexing, i.e. use all words, even the frequently occurring ones.
+# Stemming: use stemming for better textual matches. Suggestion: Porter stemming, but it is up to you to choose.
+# Important text: text in bold (b, strong), in headings (h1, h2, h3), and in titles should be treated as more important than the in other places.
+# Verify which are the relevant HTML tags to select the important words
+
+if __name__ == '__main__':
+	start()
