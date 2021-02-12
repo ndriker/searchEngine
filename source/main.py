981960eaeb5076d06b532661791f39aa5b8eaf80
Farbod <farbodghiasi@my.smccd.edu>
2021-02-11 17:45:17 -0800

    fixing inverted index

--- source/main.py
+++ source/main.py
@@ -1,27 +1,34 @@
 from bs4 import BeautifulSoup
 from nltk.stem.snowball import SnowballStemmer
-
-# import urllib.request
+from posting import Posting
 import json
 
 
 def start(inverted_index):
 	indexer(inverted_index)
+	# print(indexer(inverted_index))
+
 
 
 def indexer(inverted_index):
-
 	n = 0
-	documents = ['/home/fghiasi/M1_project/searchEngine/source/sherlock_ics_uci_edu/0c2b9a574025e46c443fee02f6269df8f573f95784713e1dbd5c2e2cd07fb6ef.json']
+	documents = ['/home/farbod/Documents/m1_proj/searchEngine/source/sherlock_ics_uci_edu/0c2b9a574025e46c443fee02f6269df8f573f95784713e1dbd5c2e2cd07fb6ef.json']
 	for document in documents:
 		n += 1
 		content = extract_json_content(document)
-
 		text = tokonize(content)
+		word_freq = computeWordFrequencies(text)
 
+		for i, token in enumerate(text):
+			if token not in inverted_index:
+				inverted_index[token] = []
+			inverted_index[token].append(Posting(n, word_freq[token], i))
+
+			print(token, " ", Posting(n, word_freq[token], i))
+
+	return inverted_index
 
 def extract_json_content(path):
-
 	file = None
 	# https://stackoverflow.com/questions/713794/catching-an-exception-while-using-a-python-with-statement
 	try:
@@ -31,7 +38,7 @@ def extract_json_content(path):
 	except EnvironmentError as error:
 		print(error, " while reading ", path)
 	if file:
-		return tokonize(file['content'])
+		return file['content']
 
 
 
@@ -45,8 +52,19 @@ def tokonize(html_content):
 	for word in soup.text.split():
 		word = stemmer.stem(word.lower())
 		tokens.append(word)
-	print(tokens)
-	print("Testing")
+	return tokens
+
+def computeWordFrequencies(tokens):
+	wordfreq = dict()
+	for token in tokens:
+		if token not in wordfreq:
+			wordfreq[token] = 1
+		else:
+			wordfreq[token] += 1
+	return wordfreq
+
+
+
 
 # Stop words: do not use stopping while indexing, i.e. use all words, even the frequently occurring ones.
 # Stemming: use stemming for better textual matches. Suggestion: Porter stemming, but it is up to you to choose.
--- source/posting.py
+++ source/posting.py
@@ -3,3 +3,6 @@ class Posting:
         self.doc_id = doc_id
         self.freq_counts = freq_counts
         self.position = position
+
+    def __str__(self):
+        return f'doc id is {self.doc_id}, token frequency is {self.freq_counts}, token position is {self.position}'
